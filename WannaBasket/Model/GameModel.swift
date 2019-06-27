080d21ac7f4dd06efd7242f461453200e7d1ecf0
het99 <gocks0922@gmail.com>
2019-06-27 22:14:47 +0900

    Create GameModel & RecordModel
    - 게임 시작, 진행, 결과를 나타내는 모델
    - 게임에서 발생하는 기록을 나타내는 모델

---
+++ WannaBasket/Model/GameModel.swift
@@ -0,0 +1,24 @@
+//
+//  GameModel.swift
+//  WannaBasket
+//
+//  Created by Het Song on 27/06/2019.
+//  Copyright © 2019 Het Song. All rights reserved.
+//
+
+import Foundation
+
+protocol GameModel {
+    
+    // Team & Player Info
+    var homeTeam: Team { get set }
+    var awayTeam: Team { get set }
+    var homeOnFloorIndexes: [Int] { get set }
+    var awayOnFloorIndexes: [Int] { get set }
+    
+    // Quarter & Time Info
+    
+    
+    // Record
+    
+}
---
+++ WannaBasket/Model/RecordModel.swift
@@ -0,0 +1,18 @@
+//
+//  RecordModel.swift
+//  WannaBasket
+//
+//  Created by Het Song on 27/06/2019.
+//  Copyright © 2019 Het Song. All rights reserved.
+//
+
+import Foundation
+
+/*
+ * 게임에서 발생하는 모든 기록을 표현할 수 있는 모델
+ * 다른 기록으로부터 독립적이어야 함
+ * 되돌릴 수 있어야함
+ */
+protocol RecordModel {
+    
+}
