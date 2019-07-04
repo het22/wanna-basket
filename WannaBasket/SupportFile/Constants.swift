1ed7619f972c92b9ad1397c394ff41a620fd2789
het99 <gocks0922@gmail.com>
2019-07-04 16:50:51 +0900

    Fix Constants.swift
    - 구조체는 원치않는 인스턴스를 생성할 수 있으므로 상수 정의에 적합하지 않다.
    - 케이스가 없는 열거형을 사용하면 인스턴스 생성을 막을 수 있다.

--- WannaBasket/SupportFile/Constants.swift
+++ WannaBasket/SupportFile/Constants.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-struct Constants {
+enum Constants {
     
-    struct Color {
+    enum Color {
         static let Black = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         static let Steel = #colorLiteral(red: 0.4756349325, green: 0.4756467342, blue: 0.4756404161, alpha: 1)
         static let Silver = #colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)
@@ -19,7 +19,7 @@ struct Constants {
         static let AwayDefault = #colorLiteral(red: 1, green: 0.3195095657, blue: 0.4566315059, alpha: 1)
     }
     
-    struct Text {
+    enum Text {
         static let HomeDefault = "홈팀"
         static let AwayDefault = "원정팀"
     }
