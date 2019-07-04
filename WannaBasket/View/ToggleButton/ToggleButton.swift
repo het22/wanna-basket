a51f657dd5f660844a9b9f5861232a64f27413cc
het99 <gocks0922@gmail.com>
2019-07-04 12:15:31 +0900

    Create ToggleButton.swift
    - 누르면 활성/비활성 상태로 토글할 수 있는 커스텀 버튼 추가

---
+++ WannaBasket/View/ToggleButton/ToggleButton.swift
@@ -0,0 +1,49 @@
+//
+//  ToggleButton.swift
+//  WannaBasket
+//
+//  Created by Het Song on 04/07/2019.
+//  Copyright © 2019 Het Song. All rights reserved.
+//
+
+import UIKit
+
+class ToggleButton: UIView, NibLoadable {
+    
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib()
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib()
+        commonInit()
+    }
+    
+    func commonInit() {
+        layer.borderColor = highlightColor.cgColor
+        layer.borderWidth = 2
+    }
+    
+    func setup(name: String, highlightColor: UIColor) {
+        nameLabel.text = name
+        self.highlightColor = highlightColor
+    }
+    
+    private var highlightColor: UIColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    private var _isHighlighted: Bool = false
+    var isHighlighted: Bool {
+        get { return _isHighlighted }
+        set(newVal) {
+            if newVal == _isHighlighted { return }
+            view.backgroundColor = newVal ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            nameLabel.textColor = newVal ? #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) : #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            _isHighlighted = newVal
+        }
+    }
+}
