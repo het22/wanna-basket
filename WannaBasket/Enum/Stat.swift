1b029a098565a5a475d705aac8aa1c422b7c3d58
het99 <gocks0922@gmail.com>
2019-07-05 15:02:28 +0900

    Create Stat Enumeration
    - 득점, 어시스트 등의 기록을 나타내는 열거형
    + 모듈 열거형 파일명 변경 ( ModuleEnum -> Module)

---
+++ WannaBasket/Enum/Stat.swift
@@ -0,0 +1,18 @@
+//
+//  Stat.swift
+//  WannaBasket
+//
+//  Created by Het Song on 05/07/2019.
+//  Copyright © 2019 Het Song. All rights reserved.
+//
+
+import Foundation
+
+enum Stat {
+    case PT1
+    case PT2
+    case PT3
+    case Assist
+    case Block
+    case Rebound
+}
