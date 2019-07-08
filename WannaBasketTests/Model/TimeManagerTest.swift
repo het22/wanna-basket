c1385d31910e33f6ebf022e17e2f3475b40524d6
het99 <gocks0922@gmail.com>
2019-07-09 01:32:56 +0900

    Create TimeManagerTest.swift

---
+++ WannaBasketTests/Model/TimeManagerTest.swift
@@ -0,0 +1,36 @@
+//
+//  TimeManagerTest.swift
+//  WannaBasketTests
+//
+//  Created by Het Song on 09/07/2019.
+//  Copyright © 2019 Het Song. All rights reserved.
+//
+
+import XCTest
+@testable import WannaBasket
+
+class TimeManagerTest: XCTestCase {
+    
+    var manager: TimeManager!
+    
+    override func setUp() {
+        manager = TimeManager(maxRegularQuarterNum: 4)
+    }
+    
+    func testCurrentTimeGetter() {
+        let currentTime = manager.currentTime
+        let time = manager.times[0]
+        XCTAssertEqual(currentTime, time)
+    }
+    
+    func testCurrentTimeSetter() {
+        manager.currentQuarter = .Regular(2)
+        manager.currentTime.gameClock = 400.0
+        manager.currentTime.shotClock = 14.0
+        let currentTime = manager.currentTime
+        let time = Time(quarter: .Regular(2),
+                        gameClock: 400.0,
+                        shotClock: 14.0)
+        XCTAssertEqual(currentTime, time)
+    }
+}
