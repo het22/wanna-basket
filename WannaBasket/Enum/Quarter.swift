24dfd9508561652ffde61b3c215dc894b5181570
het99 <gocks0922@gmail.com>
2019-07-09 00:03:59 +0900

    Update Quarter.swift
    - 쿼터타입에 연관값 추가해서 몇 쿼터인지에 대한 정보 담기
    - 쿼터타입에 CustomStringConvertible 적용
    - 쿼터에 CustomStringConvertible 적용
    - 게임시간과 공격시간 Description 구현

--- WannaBasket/Enum/Quarter.swift
+++ WannaBasket/Enum/Quarter.swift
@@ -8,12 +8,49 @@
 
 import Foundation
 
-struct Quarter {
-    enum QuarterType {
-        case Regular
-        case Overtime
-    }
+struct Quarter: CustomStringConvertible {
+    
     let type: QuarterType
+    enum QuarterType: CustomStringConvertible {
+        case Regular(Int)
+        case Overtime(Int)
+        
+        var num: Int {
+            switch self {
+            case .Regular(let num):
+                return num
+            case .Overtime(let num):
+                return num
+            }
+        }
+        
+        var description: String {
+            switch self {
+            case .Regular:
+                return "\(num+1)쿼터"
+            case .Overtime:
+                return "연장\(num+1)쿼터"
+            }
+        }
+    }
+    
     var gameClock: Float
+    var gameClockDescription: String {
+        if gameClock >= 60.0 {
+            let min = Int(gameClock) / 60
+            let sec = Int(gameClock) % 60
+            return gameClockFormat.string(from: NSNumber(integerLiteral: min))! + ":" + gameClockFormat.string(from: NSNumber(integerLiteral: sec))!
+        } else {
+            return shotClockFormat.string(from: NSNumber(value: gameClock))!
+        }
+    }
+    
     var shotClock: Float
+    var shotClockDescription: String {
+        return shotClockFormat.string(from: NSNumber(value: shotClock))!
+    }
+    
+    var description: String {
+        return "\(type.description) \(gameClockDescription) \(shotClockDescription)"
+    }
 }
