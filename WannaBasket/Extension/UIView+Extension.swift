e2b276ad108aec72ae72eb9902109de35a137981
het99 <gocks0922@gmail.com>
2019-07-09 16:02:30 +0900

    Update UIView+Extension.swift
    - 그라데이션 애니메이션 추가

--- WannaBasket/Extension/UIView+Extension.swift
+++ WannaBasket/Extension/UIView+Extension.swift
@@ -11,16 +11,56 @@ import UIKit
 extension UIView {
     
     func animateBlink(completion: @escaping (Bool)->Void) {
-        let duration = 0.08
+        let duration = 0.4
         let delay = 0.0
-        UIView.animate(withDuration: duration, delay: delay, options: [.repeat, .autoreverse], animations: {
+        UIView.animate(withDuration: duration/4, delay: delay, options: [.repeat, .autoreverse], animations: {
             self.alpha = 0.5
         }, completion: { bool in
             self.alpha = 1.0
             completion(bool)
         })
-        DispatchQueue.main.asyncAfter(deadline: .now()+0.32) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + (duration)) {
             self.layer.removeAllAnimations()
+        }
+    }
+    
+    func animateGradient(completion: @escaping (Bool)->Void) {
+        let gradient: CAGradientLayer = {
+            let gradient = CAGradientLayer()
+            gradient.frame = self.bounds
+            gradient.colors = [#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0).cgColor,#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0).cgColor]
+            gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+            gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+            return gradient
+        }()
+        self.layer.addSublayer(gradient)
+        
+        let animation = CAKeyframeAnimation(keyPath: #keyPath(CAGradientLayer.colors))
+        animation.values = [
+            [#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0).cgColor,#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0).cgColor],
+            [#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor,#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0).cgColor],
+            [#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor,#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor],
+            [#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor,#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor],
+            [#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0).cgColor,#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor],
+            [#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0).cgColor,#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0).cgColor]
+        ]
+        animation.keyTimes = [0,0.2,0.4,0.6,0.8,1.0]
+        animation.duration = 0.4
+        animation.delegate = self
+        animation.isRemovedOnCompletion = true
+        gradient.add(animation, forKey: "animation")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (animation.duration/2)) {
+            completion(true)
+        }
+    }
+}
+
+extension UIView: CAAnimationDelegate {
+    
+    public func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        if let sublayers = self.layer.sublayers {
+            sublayers.forEach { $0.removeFromSuperlayer() }
         }
     }
 }
