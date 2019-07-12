9a991d51487bf49238e227a881f58a6eb7d47d99
het99 <gocks0922@gmail.com>
2019-07-12 18:18:02 +0900

    Update PlayerFormView.swift
    - 플레이어 입력 뷰는 번호 선택 뷰를 새로운 팝업으로 띄울 수 있다.

--- WannaBasket/View/PlayerFormView/PlayerFormView.swift
+++ WannaBasket/View/PlayerFormView/PlayerFormView.swift
@@ -19,6 +19,7 @@ class PlayerFormView: UIView, NibLoadable {
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var numberButton: UIButton!
+    var number: Int?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -47,6 +48,53 @@ class PlayerFormView: UIView, NibLoadable {
                                                object: nil)
     }
     
+    private var backgroundView: UIView?
+    private var numberSelectView: NumberSelectView?
+    var isShowingNumberSelectView: Bool = false {
+        willSet(newVal) {
+            if newVal == isShowingNumberSelectView { return }
+            guard let superview = superview else { return }
+            if newVal {
+                let dismissGesture = UITapGestureRecognizerWithClosure { self.isShowingNumberSelectView = false }
+                
+                backgroundView = UIView(frame: superview.bounds)
+                backgroundView!.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.3)
+                backgroundView!.addGestureRecognizer(dismissGesture)
+                superview.addSubview(backgroundView!)
+                
+                numberSelectView = NumberSelectView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+                numberSelectView?._delegate = self
+                numberSelectView?.translatesAutoresizingMaskIntoConstraints = false
+                superview.addSubview(numberSelectView!)
+                
+                NSLayoutConstraint.activate([
+                    numberSelectView!.centerXAnchor.constraint(equalTo: superview.centerXAnchor),
+                    numberSelectView!.centerYAnchor.constraint(equalTo: superview.centerYAnchor, constant: 0),
+                    numberSelectView!.widthAnchor.constraint(equalTo: superview.widthAnchor, multiplier: 0.75),
+                    numberSelectView!.heightAnchor.constraint(equalTo: superview.heightAnchor, multiplier: 0.75)])
+            } else {
+                backgroundView?.removeFromSuperview()
+                numberSelectView?.removeFromSuperview()
+                backgroundView = nil
+                numberSelectView = nil
+            }
+        }
+    }
+    
+    @IBAction func numberButtonTapped() {
+        nameTextField.resignFirstResponder()
+        isShowingNumberSelectView = true
+    }
+    
+    @IBAction func cancelButtonTapped() {
+        delegate?.didTapPlayerFormCancelButton()
+    }
+    
+    @IBAction func completeButtonTapped() {
+        if isNameValid { delegate?.didTapPlayerFormCompleteButton(name: trimmedName, number: number) }
+        else { animateShake(completion: nil) }
+    }
+    
     var centerYConstraint: NSLayoutConstraint?
     var moveHeight: CGFloat = 0.0
     @objc func keyboardWillShow(notification: Notification) {
@@ -65,14 +113,6 @@ class PlayerFormView: UIView, NibLoadable {
         }
     }
     
-    @IBAction func numberButtonTapped() {
-        nameTextField.resignFirstResponder()
-    }
-    
-    @IBAction func cancelButtonTapped() {
-        delegate?.didTapPlayerFormCancelButton()
-    }
-    
     private var trimmedName: String? {
         return nameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
     }
@@ -82,10 +122,6 @@ class PlayerFormView: UIView, NibLoadable {
         let isValid = predicate.evaluate(with: trimmedName)
         return isValid
     }
-    @IBAction func completeButtonTapped() {
-        if isNameValid { delegate?.didTapPlayerFormCompleteButton(name: trimmedName, number: nil) }
-        else { animateShake(completion: nil) }
-    }
 }
 
 extension PlayerFormView: UITextFieldDelegate {
@@ -93,5 +129,15 @@ extension PlayerFormView: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         completeButtonTapped()
         return true
+    }
+}
+
+extension PlayerFormView: NumberSelectViewDelegate {
+    
+    func didSelectItem(at indexPath: IndexPath) {
+        number = indexPath.row
+        numberButton.setTitle((number==100) ? "00" : "\(number!)", for: .normal)
+        numberButton.setTitleColor(Constants.Color.Black, for: .normal)
+        isShowingNumberSelectView = false
     }
 }
