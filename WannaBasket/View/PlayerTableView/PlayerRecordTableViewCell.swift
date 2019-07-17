b24814f09106d49c7995c4001609c5554d9a4bae
het99 <gocks0922@gmail.com>
2019-07-17 12:21:02 +0900

    Update PlayerRecordTableViewCell.swift
    - 선수기록 셀은 선수의 점수와 점수 디테일을 보여준다.

--- WannaBasket/View/PlayerTableView/PlayerRecordTableViewCell.swift
+++ WannaBasket/View/PlayerTableView/PlayerRecordTableViewCell.swift
@@ -14,6 +14,7 @@ class PlayerRecordTableViewCell: UITableViewCell, NibLoadable, Reusable {
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var scoreDetailLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -23,8 +24,6 @@ class PlayerRecordTableViewCell: UITableViewCell, NibLoadable, Reusable {
     func commonInit() {
         layer.borderWidth = 1
         layer.borderColor = Constants.Color.Black.cgColor
-        numberLabel.layer.borderWidth = 1
-        numberLabel.layer.borderColor = Constants.Color.Black.cgColor
         selectionStyle = .none
     }
     
@@ -35,8 +34,10 @@ class PlayerRecordTableViewCell: UITableViewCell, NibLoadable, Reusable {
             hStack.addArrangedSubview(nameLabel)
             hStack.addArrangedSubview(numberLabel)
         }
+        self.highlightColor = highlightColor
         nameLabel.text = player.name
         numberLabel.text = "\(player.number)"
+        
         let score = records.reduce(0) {
             var point = 0
             if case Stat.Score(let Point) = $1.stat {
@@ -45,7 +46,21 @@ class PlayerRecordTableViewCell: UITableViewCell, NibLoadable, Reusable {
             return $0 + point
         }
         scoreLabel.text = "\(score)점"
-        self.highlightColor = highlightColor
+        
+        var pt1 = 0, pt2 = 0, pt3 = 0
+        records.forEach {
+            if case Stat.Score(let Point) = $0.stat {
+                switch Point {
+                case .One: pt1 += 1
+                case .Two: pt2 += 1
+                case .Three: pt3 += 1
+                }
+            }
+        }
+        let pt1Text = (pt1==0) ? "" : " 자유투(\(pt1)) "
+        let pt2Text = (pt2==0) ? "" : " 2점(\(pt2)) "
+        let pt3Text = (pt3==0) ? "" : " 3점(\(pt3)) "
+        scoreDetailLabel.text = (pt1==0 && pt2==0 && pt3==0) ? "-" : pt1Text + pt2Text + pt3Text
     }
     
     var highlightColor: UIColor = Constants.Color.Black {
