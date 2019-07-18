3442afc9bfc353c7d9bcb0e3299320358622243f
het99 <gocks0922@gmail.com>
2019-07-19 01:15:10 +0900

    Create RealmPlayer, RealmTeam
    - Realm 데이터베이스에 저장될 객체

--- WannaBasket/Database/Entity/RealmPlayer.swift
+++ WannaBasket/Database/Entity/RealmPlayer.swift
@@ -11,10 +11,27 @@ import RealmSwift
 class RealmPlayer: RealmObject {
     
     @objc dynamic var name: String = ""
-    @objc dynamic var _teamInfor: String = ""
+    @objc dynamic var _teamInfors: String = ""
     
-    var teamInfor: [String:Int] {
-        get { return _teamInfor.convertToDictionary() as [String:Int] }
-        set { if let json = newValue.convertToJson() { _teamInfor = json } }
+    var teamInfors: [String:Int] {
+        get { return _teamInfors.convertToDictionary() as [String:Int] }
+        set { if let json = newValue.convertToJson() { _teamInfors = json } }
+    }
+}
+
+extension Player: Persistable {
+    
+    init(realmObject: RealmPlayer) {
+        self.uuid = realmObject.uuid
+        self.name = realmObject.name
+        self.teamInfors = realmObject.teamInfors
+    }
+    
+    func realmObject() -> RealmPlayer {
+        let player = RealmPlayer()
+        player.uuid = self.uuid
+        player.name = self.name
+        player.teamInfors = self.teamInfors
+        return player
     }
 }
--- WannaBasket/Database/Entity/RealmTeam.swift
+++ WannaBasket/Database/Entity/RealmTeam.swift
@@ -11,10 +11,40 @@ import RealmSwift
 class RealmTeam: RealmObject {
     
     @objc dynamic var name: String = ""
-    @objc dynamic var _playerInfor: String = ""
+    @objc dynamic var _playerInfors: String = ""
     
-    var playerInfor: [Int:String] {
-        get { return _playerInfor.convertToDictionary() as [Int:String] }
-        set { if let json = newValue.convertToJson() { _playerInfor = json } }
+    var playerInfors: [String:Int] {
+        get { return _playerInfors.convertToDictionary() as [String:Int] }
+        set { if let json = newValue.convertToJson() { _playerInfors = json } }
+    }
+}
+
+extension Team: Persistable {
+    
+    init(realmObject: RealmTeam) {
+        self.uuid = realmObject.uuid
+        self.name = realmObject.name
+        
+        let playerDB: PlayerDB = RealmDB.shared
+        var playerInfors = [Int: Player]()
+        realmObject.playerInfors.forEach {
+            if let realmPlayer = playerDB.read(with: $0.key) {
+                let player = Player(realmObject: realmPlayer)
+                playerInfors[$0.value] = player
+            }
+        }
+        self.playerInfors = playerInfors
+    }
+    
+    func realmObject() -> RealmTeam {
+        let realmTeam = RealmTeam()
+        realmTeam.uuid = self.uuid
+        realmTeam.name = self.name
+        var playerInfors = [String:Int]()
+        self.playerInfors.forEach {
+            playerInfors[$0.value.uuid] = $0.key
+        }
+        realmTeam.playerInfors = playerInfors
+        return realmTeam
     }
 }
